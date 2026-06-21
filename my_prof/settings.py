d2c5f3bc2e824b054d853efcb6ad42ea0bf4e6e8
Muhammad Midhlaj <midhlajmk765@gmail.com>
2026-06-21 20:20:46 +0530

    Added Render files

--- my_prof/settings.py
+++ my_prof/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
