c80ac2067436081f08f639ef9920a5592c006293
Muhammad Midhlaj <midhlajmk765@gmail.com>
2026-06-22 00:24:40 +0530

    Move secrets to environment variables

--- my_prof/settings.py
+++ my_prof/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_7oze2vqvtm09ux-s88%=bsm@e$1ntf=asmy03e7ss20femn5z'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -140,7 +140,10 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'midhlajmk765@gmail.com'
-EMAIL_HOST_PASSWORD = 'qkyw wjxt gbmj hvjc'
-import os
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
+
+
+
