8119fd7001d00b94396318e9fcbccab525823976
Muhammad Midhlaj <midhlajmk765@gmail.com>
2026-06-23 03:12:26 +0530

    Move secrets to environment variables

--- static/js/nav.js
+++ static/js/nav.js
@@ -13,3 +13,32 @@ window.addEventListener("scroll", () => {
 function toggleMenu() {
     document.getElementById("navMenu").classList.toggle("show");
 }
+
+document.addEventListener("click", function(e){
+
+    const menu = document.getElementById("navMenu");
+    const btn = document.querySelector(".menu-btn");
+
+    if(
+        menu.classList.contains("show") &&
+        !menu.contains(e.target) &&
+        !btn.contains(e.target)
+    ){
+        menu.classList.remove("show");
+    }
+
+});
+
+document.addEventListener("mouseup", function(e){
+
+    const menu = document.getElementById("navMenu");
+
+    if(
+        menu.classList.contains("show") &&
+        !menu.contains(e.target) &&
+        !e.target.closest(".menu-btn")
+    ){
+        menu.classList.remove("show");
+    }
+
+});
